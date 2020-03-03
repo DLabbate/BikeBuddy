902db317935d386430f7ce7772833081af35e160
Gianluca Lepore <gianluca409@gmail.com>
2020-03-02 22:58:31 -0500

    Retrieved api key from console
    Generated API key and created project on google cloud platform console

--- app/src/main/java/com/example/bikebuddy/GPSFragment.java
+++ app/src/main/java/com/example/bikebuddy/GPSFragment.java
@@ -11,6 +11,8 @@ import androidx.fragment.app.Fragment;
 
 public class GPSFragment extends Fragment {
 
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
