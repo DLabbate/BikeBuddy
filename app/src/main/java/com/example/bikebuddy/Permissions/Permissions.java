53fc81a081b961513519697972b5af39306d021b
Gianluca Lepore <gianluca409@gmail.com>
2020-03-03 01:53:11 -0500

    Added permissions to string array

--- app/src/main/java/com/example/bikebuddy/Permissions/Permissions.java
+++ app/src/main/java/com/example/bikebuddy/Permissions/Permissions.java
@@ -7,6 +7,9 @@ public class Permissions {
     public static final String[] permissions =
             {
                     Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.BLUETOOTH
-            };
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_NETWORK_STATE
+};
 }
