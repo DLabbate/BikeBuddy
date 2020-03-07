cd760500147c3179daa3ed29dfb2bcfd56fc6f8e
Domenic Labbate <domeniclabbate@gmail.com>
2020-03-07 03:30:18 -0500

    Added methods to update speed and distance

--- app/src/main/java/com/example/bikebuddy/GPSFragment.java
+++ app/src/main/java/com/example/bikebuddy/GPSFragment.java
@@ -1,5 +1,6 @@
 package com.example.bikebuddy;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,6 +12,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -20,15 +26,29 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
 
-public class GPSFragment extends Fragment implements OnMapReadyCallback {
+public class GPSFragment extends Fragment implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     public static final String TAG = "GPSfragment";
+    private static final long INTERVAL = 5000;
+    private static final long FASTEST_INTERVAL = 1000;
 
     MapView gMapView;
     private GoogleMap gMap = null;
 
 
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    LocationRequest mLocationRequest;
 
+    LatLng lastKnownLatLng;
+    double curTime= 0;
+    double oldLat = 0.0;
+    double oldLon = 0.0;
+    double workout_distance = 0.0;
+    long EARTH_RADIUS = 6356 *1000;
 
     @Nullable
     @Override
@@ -42,6 +62,18 @@ public class GPSFragment extends Fragment implements OnMapReadyCallback {
         gMapView.onResume();
 
         gMapView.getMapAsync(this);
+
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+        Log.d(TAG,"oncreate distance: " + workout_distance);
+        workout_distance = 0.0;
 
         return view;
 
@@ -68,21 +100,27 @@ public class GPSFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onStart() {
         super.onStart();
+        mGoogleApiClient.connect();
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        if (mGoogleApiClient.isConnected()) {
+            createLocationRequest();
+        }
     }
 
     @Override
     public void onStop() {
         super.onStop();
+        mGoogleApiClient.disconnect();
     }
 
     @Override
     public void onPause() {
         super.onPause();
+        stopLocationUpdates();
     }
 
     @Override
@@ -94,4 +132,100 @@ public class GPSFragment extends Fragment implements OnMapReadyCallback {
     public void onLowMemory() {
         super.onLowMemory();
     }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        createLocationRequest();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lastKnownLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+        getspeed(location);
+        //incrementWorkoutDistance(location);
+        Log.d(TAG,"Latitude: " + lastKnownLatLng.latitude + " Longitude" + lastKnownLatLng.longitude);
+        Log.d(TAG,"Workout Distance: " + workout_distance);
+
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+    }
+
+    private void getspeed(Location location){
+        double newTime= System.currentTimeMillis();
+        double newLat = location.getLatitude();
+        double newLon = location.getLongitude();
+        //if(location.hasSpeed()){
+        //    float speed = location.getSpeed();
+        //    Toast.makeText(getActivity(),"SPEED : "+String.valueOf(speed)+"m/s",Toast.LENGTH_SHORT).show();
+        //}
+        //else
+            {
+            double distance = calculationBydistance(newLat,newLon,oldLat,oldLon);
+            Log.d(TAG,"DELTA DISTANCE: " + distance);
+
+
+                float[] distanceResults = new float[1];
+                Location.distanceBetween(oldLat, oldLon,
+                        newLat, newLon, distanceResults);
+
+                Log.d(TAG,"DISTANCE BETWEEN: " + distanceResults[0]);
+
+            if (oldLon != 0 && oldLat != 0)
+                workout_distance += distanceResults[0];
+            double timeDifferent = newTime - curTime;
+            double speed = distance/timeDifferent;
+            curTime = newTime;
+            oldLat = newLat;
+            oldLon = newLon;
+            Toast.makeText(getActivity(),"SPEED 2 : "+String.valueOf(speed)+"m/s",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //https://stackoverflow.com/questions/20398898/how-to-get-speed-in-android-app-using-location-or-accelerometer-or-some-other-wa
+    public double calculationBydistance(double lat1, double lon1, double lat2, double lon2){
+        double radius = EARTH_RADIUS;
+        double dLat = Math.toRadians(lat2-lat1);
+        double dLon = Math.toRadians(lon2-lon1);
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+                        Math.sin(dLon/2) * Math.sin(dLon/2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        return radius * c;
+    }
+
+    private void incrementWorkoutDistance(Location location)
+    {
+        double lat2 = location.getLatitude();
+        double lon2 = location.getLongitude();
+
+        double delta = calculationBydistance(oldLat,oldLon,lat2,lon2);
+        oldLat = lat2; //Update old values
+        oldLon = lon2;
+        workout_distance += delta;
+    }
+
+
+
 }
