c422eb08a8e8fe6ca05c2fcd3489f901283233c4
Brady <bdeslippe@outlook.com>
2020-03-16 22:41:58 -0400

    CALORIC ESTIMATE: - added the kalman filter to RecordingService - initialized during creation of the service - estimates rely on previous estimate and calculated deviance (sigma) - filter returns double[] {estimate, sigma} - Still need to add most recent estimat to workout on completion - Still need to import profile data (hardcorded for now)

--- app/src/main/java/com/example/bikebuddy/Services/RecordingService.java
+++ app/src/main/java/com/example/bikebuddy/Services/RecordingService.java
@@ -14,7 +14,6 @@ import com.example.bikebuddy.Data.DbHelper;
 import com.example.bikebuddy.FitnessFragment;
 import com.example.bikebuddy.MainActivity;
 import com.example.bikebuddy.R;
-import com.example.bikebuddy.Services.LocationService;
 import com.example.bikebuddy.Utils.Workout;
 
 import java.util.ArrayList;
@@ -36,7 +35,20 @@ public class RecordingService extends Service {
     private double averageHR;
     private double averageSpeed;
 
+
+
     private Workout workout;
+    //******************************************************************************************************
+
+
+    //Values for Kalman filter
+    //******************************************************************************************************
+    private double[] kalReturn = new double[2];     //containts estimate and deviation from kalman filter
+    double C = 1.5;                     //Kalman filter parameter
+    double Q = 0.5;                     //Kalman filter parameter
+    private int initialRate = 1;        //1=first time getting estimate, 0=first estimate complete
+    private double calRateEstimate = 0; //current best approximation of rate
+    private double sigma = Q;           //Variable Kalman filter parameter
     //******************************************************************************************************
 
 
@@ -131,6 +143,14 @@ public class RecordingService extends Service {
                 + " Distance " + LocationService.WORKOUT_DISTANCE
                 + " )");
 
+        //Here we calculate the caloric output
+        //*****************************************************************************************************************************
+            kalReturn = calculateCalorieRate(MainActivity.HR_RT, LocationService.SPEED_RT, calRateEstimate, sigma, Q, C);
+            calRateEstimate = kalReturn[0];
+            sigma = kalReturn[1];
+            //TODO: insert calRateEstimate into workout on completion of recording
+        //*****************************************************************************************************************************
+
         //Now we want to fill all the workout data
         //*****************************************************************************************************************************
         time.add((SystemClock.elapsedRealtime() - FitnessFragment.chronometer.getBase())/1000);         //Time in seconds
@@ -155,5 +175,56 @@ public class RecordingService extends Service {
         dbHelper.insertWorkout(workout);
     }
 
+    private double[] calculateCalorieRate(double heartRate, double speed, double estimate, double sigma, double Q, double C){
+        /*
+        Returns value of best guess for power output. Uses velocity data to approximate power
+        based on MET standards and then HR to approximate output based on Keytel approximation.
+        The MET standards typically provide approximations that are conservatively large. Keytel
+        approximation will be used to bring the output to a more appropriate level (matching
+        clinical trials).
+         */
+
+        //*****************************************************************************************************************************
+        //TODO: THIS DATA NEEDS TO BE TAKEN FROM PROFILE
+        //      hardcoded for now
+        int age = 27;           //years
+        int weight = 95;        //kg
+        Boolean gender = true;  //true = male, false = female
+        //*****************************************************************************************************************************
+
+        //Internal Parameters
+        double deltaTime = 10;
+        double K;
+        double MET;
+        double newVal;
+        double[] kalReturn;
+
+        //Calculating MET level
+        if(speed < 11) MET = 4.8;
+        else if(speed <16) MET = 5.9;
+        else if(speed <21) MET = 7.1;
+        else if(speed <26) MET = 8.4;
+        else MET = 9.8;
+
+        //*****************************************************************************************************************************
+        //Performing the filter approximation.
+        //  assume keytelPower only
+        newVal = ((-55.0969 + (0.6309*heartRate) + (0.1988*weight) + (0.2017*age))/4.184)*(deltaTime)*(1/60);  //kcal/min
+        K = (sigma * C)/(sigma*sigma * C*C + Q*Q);
+        estimate = estimate + K*(newVal-C*estimate);
+        sigma = (1-K*C)*sigma;
+
+        //  update with metPower
+        newVal = MET*weight/60;     //kcal/min
+        K = (sigma * C)/(sigma*sigma * C*C + Q*Q);
+        estimate = estimate + K*(newVal-C*estimate);
+        sigma = (1-K*C)*sigma;
+        //*****************************************************************************************************************************
+
+        //Returning estimate and deviation, both used at next method call
+        kalReturn = new double[]{estimate, sigma};
+        return kalReturn;
+
+    }
 
 }
