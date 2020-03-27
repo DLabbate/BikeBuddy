d14d07c36716baaad92e801670cee12e0ae136ad
Brady <bdeslippe@outlook.com>
2020-03-27 19:55:12 -0400

    - filter button works - WORKOUTS CANNOT BE CLICKED - need to change how workoutActivity calls its data

--- app/src/main/java/com/example/bikebuddy/FilterWorkoutFragment.java
+++ app/src/main/java/com/example/bikebuddy/FilterWorkoutFragment.java
@@ -1,6 +1,7 @@
 package com.example.bikebuddy;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,7 +15,6 @@ import androidx.fragment.app.DialogFragment;
 
 import com.example.bikebuddy.Data.DbHelper;
 
-import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -22,11 +22,13 @@ public class FilterWorkoutFragment extends DialogFragment {
 
     private static final String TAG = "FilterWorkoutFragment";
     private Context context;
+    private DialogInterface.OnDismissListener onDismissListener;
 
     //Fragment Objects
     Button filter1week;
     Button filter2week;
     Button filter1Month;
+    Button showAll;
 
     //Database
     DbHelper dbHelper;
@@ -55,55 +57,55 @@ public class FilterWorkoutFragment extends DialogFragment {
         filter1week = view.findViewById(R.id.button_filter1week);
         filter2week = view.findViewById(R.id.button_filter2weeks);
         filter1Month = view.findViewById(R.id.button_filter1month);
+        showAll = view.findViewById(R.id.button_showAll);
 
         //Filter buttons
-        filter1week.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTime(Calendar.getInstance().getTime());
-                calendar.add(Calendar.DATE,-7);
-                Date lowerDate = calendar.getTime();
-                Log.d(TAG,"LowerDate = " + lowerDate);
-                try {
-                    dbHelper.filterWorkoutByDate(lowerDate);
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-                getDialog().dismiss();
-            }
+        filter1week.setOnClickListener(v -> {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(Calendar.getInstance().getTime());
+            calendar.add(Calendar.DATE,-7);
+            Date lowerDate = calendar.getTime();
+            Log.d(TAG,"LowerDate = " + lowerDate);
+            LogFragment.setLowerDate(lowerDate);
+            getDialog().dismiss();
         });
-        filter2week.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTime(Calendar.getInstance().getTime());
-                calendar.add(Calendar.DATE,-14);
-                Date lowerDate = calendar.getTime();
-                Log.d(TAG,"LowerDate = " + lowerDate);
-                try {
-                    dbHelper.filterWorkoutByDate(lowerDate);
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-                getDialog().dismiss();
-            }
+        filter2week.setOnClickListener(v -> {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(Calendar.getInstance().getTime());
+            calendar.add(Calendar.DATE,-14);
+            Date lowerDate = calendar.getTime();
+            Log.d(TAG,"LowerDate = " + lowerDate);
+            LogFragment.setLowerDate(lowerDate);
+            getDialog().dismiss();
         });
-        filter1Month.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTime(Calendar.getInstance().getTime());
-                calendar.add(Calendar.DATE,-30);
-                Date lowerDate = calendar.getTime();
-                Log.d(TAG,"LowerDate = " + lowerDate);
-                try {
-                    dbHelper.filterWorkoutByDate(lowerDate);
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-                getDialog().dismiss();
-            }
+        filter1Month.setOnClickListener(v -> {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(Calendar.getInstance().getTime());
+            calendar.add(Calendar.DATE,-30);
+            Date lowerDate = calendar.getTime();
+            Log.d(TAG,"LowerDate = " + lowerDate);
+            LogFragment.setLowerDate(lowerDate);
+            getDialog().dismiss();
         });
+        showAll.setOnClickListener(v -> {
+            Date lowerDate = new Date();
+            lowerDate.setYear(19);
+            lowerDate.setMonth(11);
+            lowerDate.setDate(1);
+            LogFragment.setLowerDate(lowerDate);
+            getDialog().dismiss();
+        });
+    }
+
+    public void setOnDismissListener(DialogInterface.OnDismissListener onDismissListener) {
+        this.onDismissListener = onDismissListener;
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
+        if (onDismissListener != null) {
+            onDismissListener.onDismiss(dialog);
+        }
     }
 }
--- app/src/main/java/com/example/bikebuddy/LogFragment.java
+++ app/src/main/java/com/example/bikebuddy/LogFragment.java
@@ -1,5 +1,6 @@
 package com.example.bikebuddy;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,15 +18,21 @@ import com.example.bikebuddy.Utils.Workout;
 import com.example.bikebuddy.Utils.WorkoutAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.text.ParseException;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 public class LogFragment extends Fragment {
 
     private static final String TAG = "LogFragment";
     private DbHelper dbHelper;
-    //Filter button
+
+    //Filter functionality
     FloatingActionButton FAB_filterByDate;
+    static Date lowerDate = new Date();
+    FilterWorkoutFragment d = new FilterWorkoutFragment();
+
     //RecyclerView
     RecyclerView workoutRecyclerView;
     RecyclerView.Adapter workoutAdapter;
@@ -40,40 +47,83 @@ public class LogFragment extends Fragment {
     long longvalue=10;
 
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        d.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                Log.d(TAG,"onDismiss");
+                refreshList();
+            }
+        });
+        lowerDate.setYear(19);
+        lowerDate.setMonth(11);
+        lowerDate.setDate(1);
+        dbHelper = new DbHelper(this.getActivity());
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_logs,container,false);
+        Log.d(TAG,"onCreateView");
 
         workoutRecyclerView = view.findViewById(R.id.recycler_view_workout);
-
         FAB_filterByDate = view.findViewById(R.id.FAB_filter_workouts);
         setupFAB();
 
-        dbHelper = new DbHelper(this.getActivity());
-        testStrings = dbHelper.getWorkouts();
+        //testStrings = dbHelper.getWorkouts();
       //  testStrings = new LinkedList<Workout>();
       //  testStrings.add(new Workout(longList,doublelist,doublelist,doublevalue,longvalue,doublevalue,doublevalue,doublevalue));
       //  testStrings.add("Test2");
       //  testStrings.add("Test3");
+        refreshList();
+        /*
         Log.d(TAG,"testStrings size: " + testStrings.size());
         workoutAdapter = new WorkoutAdapter(getActivity(),testStrings);
         linearLayoutManager = new LinearLayoutManager(getActivity());
         workoutRecyclerView.setAdapter(workoutAdapter);
         workoutRecyclerView.setLayoutManager(linearLayoutManager);
-
+        */
         return view;
     }
+
+
+    //Method that can be called from child fragment to pass date value.
+    static void setLowerDate(Date date){
+        Log.d(TAG,"setLowerDate: " + date);
+        lowerDate = date;
+    }
+
 
     private void setupFAB()
     {
         FAB_filterByDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                d.show(getFragmentManager(),"Filter Workouts Fragment");
+                /*
                 FilterWorkoutFragment filterWorkoutFragment = new FilterWorkoutFragment();
                 filterWorkoutFragment.show(getFragmentManager(),"Filter Workouts Fragment");
+                 */
             }
         });
     }
+
+    private void refreshList(){
+        Log.d(TAG,"Refresh List");
+        List<Workout> filteredWorkoutList;
+        try {
+            filteredWorkoutList = dbHelper.filterWorkoutByDate(lowerDate);
+            workoutAdapter = new WorkoutAdapter(this.getContext(),filteredWorkoutList);
+            linearLayoutManager = new LinearLayoutManager(getActivity());
+            workoutRecyclerView.setAdapter(workoutAdapter);
+            workoutRecyclerView.setLayoutManager(linearLayoutManager);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
